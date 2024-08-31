d9bdae8798e9808fcdd25f18d54d07d48897889f
KingHaate <samuelsamagaia13@gmail.com>
2024-08-31 09:45:55 -0300

    First commit

---
+++ Aula 1 - IntroduçãoOO&TDD/lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+
+  def talk(world = '')
+    return 'Hello World, Ruby!' if world.empty?
+
+    "#{world}, Ruby!"
+  end
+end
---
+++ Aula 1 - IntroduçãoOO&TDD/spec/person_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'person'
+
+RSpec.describe Person do
+  describe '#talk' do
+    it 'can talk' do
+      person = Person.new
+      expect(person.talk).to eq('Hello World, Ruby!')
+    end
+
+    it 'can talks others languages' do
+      person = Person.new
+      expect(person.talk('Olá mundo')).to eq('Olá mundo, Ruby!')
+    end
+  end
+end
